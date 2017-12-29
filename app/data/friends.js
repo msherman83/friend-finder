f7aebd47cf03f96ad8ab5edfe0b8807301eb2c2b
msherman83 <msherman83@gmail.com>
2017-12-29 17:01:56 -0500

    cleaned up apiRoutes

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,17 +3,17 @@
 var friendsArray = [
     {
         name: "Mike",
-        Photo: "https://i.pinimg.com/originals/e9/f7/34/e9f734f3ee560228b479e87c53a4caac.jpg",
+        photo: "https://i.pinimg.com/originals/e9/f7/34/e9f734f3ee560228b479e87c53a4caac.jpg",
         scores: "1353343345"
     },
     {
         name: "Jim",
-        Photo: "http://kikiandtea.com/wp-content/uploads/nerd.jpg",
+        photo: "http://kikiandtea.com/wp-content/uploads/nerd.jpg",
         scores: "4333433325"
     },
     {
         name: "Billy",
-        Photo: "http://kikiandtea.com/wp-content/uploads/nerd.jpg",
+        photo: "http://kikiandtea.com/wp-content/uploads/nerd.jpg",
         scores: "2455533311"
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,29 +2,13 @@ var friendsData = require("../data/friends.js");
 
 
 module.exports = function (app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function (req, res) {
     return res.json(friendsData);
   });
 
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
   app.post("/api/friends", function (req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
 
     var bestFriend = {
       name: "",
@@ -34,71 +18,40 @@ module.exports = function (app) {
 
     console.log(friendsData)
 
-    var inputData = req.body;
-    var inputScores = req.body.scores;
-    var totalDiff = 0;
+    var surveyScores = req.body.scores;
+    var totalDifference = 0;
 
     console.log("console logging scores " + req.body.scores);
 
     for (var i = 0; i < friendsData.length; i++) {
 
       // console.log("Input Data" + inputData);
-      console.log("Input Scores" + inputScores);
-      console.log("total Diff" + totalDiff);
-      totalDiff = 0;
+      console.log("Input Scores" + surveyScores);
+      console.log("total Diff" + totalDifference);
+      totalDifference = 0;
 
       //Loop through the scores of each friend
       for (var j = 0; j < friendsData[i].scores[j]; j++) {
-          //calculating the difference between each score and sum them into totalDifference
-          totalDiff += Math.abs(parseInt(inputScores[j]) - parseInt(friendsData[i].scores[j]));
+        //calculating the difference between each score and sum them into totalDifferenceerence
+        totalDifference += Math.abs(parseInt(surveyScores[j]) - parseInt(friendsData[i].scores[j]));
 
-          console.log("best friend " + bestFriend.friendDiff);
-          //Find best friend match
-          if (totalDiff <= bestFriend.friendDiff) {
+        console.log("best friend " + bestFriend.friendDiff);
+        //Find best friend match
+        if (totalDifference <= bestFriend.friendDiff) {
 
-              bestFriend.name = friendsData[i].name;
-              bestFriend.photo = friendsData[i].photo;
-              bestFriend.friendDiff = totalDiff;
+          bestFriend.name = friendsData[i].name;
+          bestFriend.photo = friendsData[i].photo;
+          bestFriend.friendDiff = totalDifference;
 
-          }
+        }
       }
-  }
+    }
 
 
-  //Pushing new friend to friends API
-  friendsData.push(inputData);
+    // push new friend to /api/friends
+    friendsData.push(req.body);
 
-  res.json(bestFriend);
+    res.json(bestFriend);
   });
 
-  //   if (friendsData.length) {
-  //     friendsData.push(req.body);
-  //   } else {
-  //     console.log("Nothing to push!");
-  //     console.log(friendsData)
-  //   }
-  // });
-
-  // app.post("/data/friendsData", function (req, res) {
-  //   // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-  //   // It will do this by sending out the value "true" have a table
-  //   // req.body is available since we're using the body-parser middleware
-  //   if (friendsData.length) {
-  //     friendsData.push(req.body);
-  //   } else {
-  //     console.log("Nothing to push!");
-  //     console.log(friendsData)
-  //   }
-  // });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  // app.post("/api/clear", function() {
-  //   // Empty out the arrays of data
-  //   friendsData = [];
-
-  //   console.log(friendsData);
-  // });
 };
