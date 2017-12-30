c650b0746e757246f48a6806ef0cf4ad7b379106
msherman83 <msherman83@gmail.com>
2017-12-30 10:55:28 -0500

    css weirdness

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static(path.join(__dirname, "/public")));
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
